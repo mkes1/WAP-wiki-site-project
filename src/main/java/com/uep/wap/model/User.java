16f95a3d9d4180d654c3359997bef07836a55937
Dawid Bojda <83711@student.ue.poznan.pl>
2023-05-10 12:24:57 +0200

    added user entity

---
+++ src/main/java/com/uep/wap/model/User.java
@@ -0,0 +1,63 @@
+package com.uep.wap.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="users")
+public class User{
+    @Id
+    @Column(name ="id")
+    private long id;
+    @Column(name ="username")
+    private String username;
+
+
+
+    @Column(name ="password")
+    private String password;
+    @Column(name ="role")
+    private String role;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    public void setId(long id){
+        this.id = id;
+    }
+    public long getId(){
+        return id;
+    }
+    public User(){
+
+    }
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+    public User(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    }
+
+
+
