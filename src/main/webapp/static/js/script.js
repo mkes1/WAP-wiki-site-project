500f78a643b5fd24f9a0a0a64bdfb8392ebc139d
Mateusz Kęska <mateuszkeska@192.168.1.10>
2023-06-18 20:44:11 +0200

    connection of db and frontend

--- src/main/webapp/static/js/script.js
+++ src/main/webapp/static/js/script.js
@@ -1,56 +1,44 @@
-// Function to fetch and display page details
-function fetchPageDetails(pageId) {
-    // Fetch page details from the backend API
-    fetch('/pages/' + pageId)
-        .then(response => response.json())
-        .then(page => {
-            // Update page details on the page
-            document.getElementById('pageId').textContent = page.id;
-            document.getElementById('pageTitle').textContent = page.title;
-            document.getElementById('pageContent').textContent = page.content;
-            document.getElementById('pageSectionId').textContent = page.sectionId;
-        })
-        .catch(error => console.error('Error:', error));
+/*jshint esversion: 6 */
+
+function fetchPageDetails() {
+  // Fetch and display the list of pages
+  fetch('/pages')
+    .then(response => response.json())
+    .then(pages => {
+      var pagesList = document.getElementById('pagesList');
+
+      pages.forEach(page => {
+        var pageLink = document.createElement('a');
+        pageLink.href = page.html?page_id=${page.id};
+        pageLink.textContent = page.title;
+
+        var listItem = document.createElement('li');
+        listItem.appendChild(pageLink);
+
+        pagesList.appendChild(listItem);
+      });
+    })
+    //.catch(error => console.error('Error:', error));
 }
 
 // Function to fetch and display section details
-function fetchSectionDetails(sectionId) {
-    // Fetch section details from the backend API
-    fetch('/sections/' + sectionId)
-        .then(response => response.json())
-        .then(section => {
-            // Update section details on the page
-            document.getElementById('sectionId').textContent = section.id;
-            document.getElementById('sectionTitle').textContent = section.title;
-            document.getElementById('sectionParentId').textContent = section.parentId;
-        })
-        .catch(error => console.error('Error:', error));
+function fetchSectionDetails() {
+  // Fetch section details from the backend API
+  fetch('/sections/{section_id}')
+    .then(response => response.json())
+    .then(section => {
+      // Update section details on the page
+      document.getElementById('sectionTitle').textContent = section.title;
+      document.getElementById('sectionContent').textContent = section.content;
+    })
+    //.catch(error => console.error('Error:', error));
 }
 
-// Function to fetch and display media file details
-function fetchMediaFileDetails(mediafileId) {
-    // Fetch media file details from the backend API
-    fetch('/mediafiles/' + mediafileId)
-        .then(response => response.json())
-        .then(mediafile => {
-            // Update media file details on the page
-            document.getElementById('mediafileId').textContent = mediafile.id;
-            document.getElementById('mediafileTitle').textContent = mediafile.title;
-            document.getElementById('mediafileType').textContent = mediafile.type;
-            document.getElementById('mediafileUrl').textContent = mediafile.url;
-        })
-        .catch(error => console.error('Error:', error));
-}
+
 
 // Call the fetch functions on page load
 window.onload = function () {
-    const pageId = 1; // Replace with the actual page ID
-    const sectionId = 1; // Replace with the actual section ID
-    const mediafileId = 1; // Replace with the actual media file ID
-    const userId = 1; // Replace with the actual user ID
+  fetchPageDetails();
+  fetchSectionDetails();
 
-    fetchPageDetails(pageId);
-    fetchSectionDetails(sectionId);
-    fetchMediaFileDetails(mediafileId);
-    fetchUserDetails(userId);
 };
